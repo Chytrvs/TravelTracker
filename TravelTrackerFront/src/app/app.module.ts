7a7979d9ca04edfeee4f8621e19d82d6e1a344ee
Chytrvs <marcinchytrzynski@gmail.com>
2019-04-28 22:39:19 +0200

    Add auth service

--- TravelTrackerFront/src/app/app.module.ts
+++ TravelTrackerFront/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PathComponent } from './path/path.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from 'src/services/auth.service';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      PathComponent
+      PathComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [AuthService],
    bootstrap: [
       AppComponent
    ]
