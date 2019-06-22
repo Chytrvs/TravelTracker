579521777bbd18ccc219c11c2000f3946f4038b1
Chytrvs <marcinchytrzynski@gmail.com>
2019-06-22 14:59:19 +0200

    Implemented AddFlight method

--- TravelTrackerFront/src/app/flight/flight.component.ts
+++ TravelTrackerFront/src/app/flight/flight.component.ts
@@ -1,27 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
-  selector: 'app-flight',
-  templateUrl: './flight.component.html',
-  styleUrls: ['./flight.component.css']
+  selector: "app-flight",
+  templateUrl: "./flight.component.html",
+  styleUrls: ["./flight.component.css"]
 })
 export class FlightComponent implements OnInit {
-  airports:any;
-  constructor(private http: HttpClient) { }
+  airports: any;
+  model: any={};
+  constructor(private http: HttpClient,private auth: AuthService) {}
 
   ngOnInit() {
     this.getAirports();
   }
-  getAirports(){
-    this.http.get('http://localhost:5000/api/Trips/GetAirports').subscribe(response=>{
-      this.airports=response;
-    },error=>{
-      console.log(error);
-    })
+  getAirports() {
+    this.http.get("http://localhost:5000/api/Trips/GetAirports").subscribe(
+      response => {
+        this.airports = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
-  addFlight(){
-
+  addFlight() {
+    this.http
+      .post("http://localhost:5000/api/Trips/AddFlight", {
+        Username: this.auth.decodedToken.unique_name,
+        DepartureAirportAcronym: this.model.departureAirport,
+        DestinationAirportAcronym: this.model.destinationAirport
+      })
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log("Error", error);
+        }
+      );
   }
-
 }
