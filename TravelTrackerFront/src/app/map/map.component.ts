b0e88121514a941833dbbce7390fbd03baec1373
Chytrvs <marcinchytrzynski@gmail.com>
2019-06-29 12:28:25 +0200

    Add distance traveled measurement

--- TravelTrackerFront/src/app/map/map.component.ts
+++ TravelTrackerFront/src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import * as arcjs from 'node_modules/arc/arc.js';
 import * as Collections from 'typescript-collections';
+import * as sphere from 'node_modules/ol/sphere.js';
 
 import OlMap from 'ol/Map';
 import OlOSM from 'ol/source/OSM.js';
@@ -12,6 +13,8 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import Vector from 'ol/source/Vector.js';
 import Style from 'ol/style/Style.js';
 import Stroke from 'ol/style/stroke.js';
+
+
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { AuthService } from 'src/services/auth.service';
 import { AlertifyService } from 'src/services/alertify.service';
@@ -33,15 +36,21 @@ export class MapComponent implements OnInit {
   style: Style;
   stroke: Stroke;
 
+ 
+
+
   private options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
   flights: any;
   NumberOfFlights: number;
   NumberOfAirportsVisited:number;
+  DistanceTraveled:number;
 
   constructor(private http: HttpClient,private auth: AuthService,private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.NumberOfFlights=0;
+    this.NumberOfAirportsVisited=0;
+    this.DistanceTraveled=0;
     this.getFlights();
     
   }
@@ -64,16 +73,19 @@ export class MapComponent implements OnInit {
 
   }
   GenerateCurves(flightsData: any){
+    var distance=sphere.getDistance([0,0],[180,0]);
+    console.log(distance);
     var AirportsVisited=new Collections.Set(String);
     this.vector=new Vector();
     this.format=new GeoJSON({
       featureProjection:"EPSG:3857"
     })
     flightsData.forEach(Flight => {
-      
+
       AirportsVisited.add(Flight.departureAirport.acronym);
       AirportsVisited.add(Flight.destinationAirport.acronym);
       this.NumberOfFlights++;
+      this.DistanceTraveled+=sphere.getDistance([Flight.departureAirport.longitude,Flight.departureAirport.latitude],[Flight.destinationAirport.longitude,Flight.destinationAirport.latitude]);
       var generator = new arcjs.GreatCircle({x: Flight.departureAirport.longitude, y: Flight.departureAirport.latitude},{x: Flight.destinationAirport.longitude, y: Flight.destinationAirport.latitude});
       var n = 50; // n of points
       var coords = generator.Arc(n).geometries[0].coords;
