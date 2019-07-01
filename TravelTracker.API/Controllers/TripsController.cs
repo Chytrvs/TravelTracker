de2227b31d3dfe42c9c3c46a7915417bacd7a4ba
Chytrvs <marcinchytrzynski@gmail.com>
2019-07-01 17:01:03 +0200

    Changed GetUserFlights to GET request

--- TravelTracker.API/Controllers/TripsController.cs
+++ TravelTracker.API/Controllers/TripsController.cs
@@ -6,16 +6,12 @@ using TravelTracker.API.Data.DataModels;
 using TravelTracker.API.Data.DataTransferObjects;
 using TravelTracker.API.Data.Repositories;
 
-//TODO Make sure each flight is attached to a user
-//TODO Add action to return user trips
-//TODO Add some kind of error system to know what went wrong in a repository
-
 
 namespace TravelTracker.API.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class TripsController:ControllerBase
+    public class TripsController : ControllerBase
     {
         private readonly ITripRepository _repository;
 
@@ -23,37 +19,45 @@ namespace TravelTracker.API.Controllers
         {
             _repository = repository;
         }
-        public async Task<IActionResult> AddFlight(FlightRequestDTO flightRequestDTO){
-            var flight =await _repository.AddFlight(flightRequestDTO);
-            if(flight!=null){
+        public async Task<IActionResult> AddFlight(FlightRequestDTO flightRequestDTO)
+        {
+            var flight = await _repository.AddFlight(flightRequestDTO);
+            if (flight != null)
+            {
                 return Ok(flight);
             }
             return BadRequest("One of the airports doesnt exist!");
         }
-        public async Task<IActionResult> AddAirport(Airport airport){
-            Airport newairport =await _repository.AddAirport(airport);
-            if(newairport!=null){
+        public async Task<IActionResult> AddAirport(Airport airport)
+        {
+            Airport newairport = await _repository.AddAirport(airport);
+            if (newairport != null)
+            {
                 return Ok(newairport);
             }
             return BadRequest("Airport already exists!");
         }
-        [HttpPost]
-        public async Task<IActionResult> GetUserFlights(UsernameDTO usernameDTO){
-            var res=await _repository.GetUserFlights(usernameDTO.Username);
-            if(res!=null){
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetUserFlights(string username)
+        {
+            var res = await _repository.GetUserFlights(username);
+            if (res != null)
+            {
                 return Ok(res);
             }
             return BadRequest("Flights cannot be found.");
-            
+
         }
         [HttpGet]
-        public async Task<IActionResult> GetAirports(){
-            var res=await _repository.GetAirports();
-            if(res!=null){
+        public async Task<IActionResult> GetAirports()
+        {
+            var res = await _repository.GetAirports();
+            if (res != null)
+            {
                 return Ok(res);
             }
             return BadRequest("Airports cannot be found.");
-            
+
         }
     }
 }
--- TravelTrackerFront/src/app/map/map.component.ts
+++ TravelTrackerFront/src/app/map/map.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import * as arcjs from 'node_modules/arc/arc.js';
-import * as Collections from 'typescript-collections';
-import * as sphere from 'node_modules/ol/sphere.js';
+import * as arcjs from "node_modules/arc/arc.js";
+import * as Collections from "typescript-collections";
+import * as sphere from "node_modules/ol/sphere.js";
 
-import OlMap from 'ol/Map';
-import OlOSM from 'ol/source/OSM.js';
-import OlVectorLayer from 'ol/layer/Vector.js';
-import OlTileLayer from 'ol/layer/Tile.js';
-import OlView from 'ol/View';
-import GeoJSON from 'ol/format/GeoJSON.js';
-import Vector from 'ol/source/Vector.js';
-import Style from 'ol/style/Style.js';
-import Stroke from 'ol/style/stroke.js';
+import OlMap from "ol/Map";
+import OlOSM from "ol/source/OSM.js";
+import OlVectorLayer from "ol/layer/Vector.js";
+import OlTileLayer from "ol/layer/Tile.js";
+import OlView from "ol/View";
+import GeoJSON from "ol/format/GeoJSON.js";
+import Vector from "ol/source/Vector.js";
+import Style from "ol/style/Style.js";
+import Stroke from "ol/style/stroke.js";
 
-
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-import { AuthService } from 'src/services/auth.service';
-import { AlertifyService } from 'src/services/alertify.service';
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { AuthService } from "src/services/auth.service";
+import { AlertifyService } from "src/services/alertify.service";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"]
 })
-
 export class MapComponent implements OnInit {
   map: OlMap;
   source: OlOSM;
@@ -36,77 +34,98 @@ export class MapComponent implements OnInit {
   style: Style;
   stroke: Stroke;
 
-  private options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
+  private options = {
+    headers: new HttpHeaders().set("Content-Type", "application/json")
+  };
   flights: any;
   NumberOfFlights: number;
-  NumberOfAirportsVisited:number;
-  DistanceTraveled:any;
-  
+  NumberOfAirportsVisited: number;
+  DistanceTraveled: any;
 
-  constructor(private http: HttpClient,private auth: AuthService,private alertify: AlertifyService) { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {
-    this.NumberOfFlights=0;
-    this.NumberOfAirportsVisited=0;
-    this.DistanceTraveled=0;
+    this.NumberOfFlights = 0;
+    this.NumberOfAirportsVisited = 0;
+    this.DistanceTraveled = 0;
     this.getFlights();
-    
   }
-  getFlights(){
-    this.http.post("http://localhost:5000/api/Trips/GetUserFlights",
-    {
-    "Username":  this.auth.decodedToken.unique_name,
-    })
+  getFlights() {
+    this.http.get("http://localhost:5000/api/Trips/GetUserFlights/"+this.auth.decodedToken.unique_name)
     .subscribe(
-    data  => {
-      this.alertify.success("Successfully loaded your flights");
-    this.DrawMap(this.GenerateCurves(data));
-    },
-    error  => {
-    this.alertify.message("In order to see your flights displayed on a map, add some of them in a first place");
-    this.DrawMap(new Vector());
-    }
+      data => {
+        this.alertify.success("Successfully loaded your flights");
+        this.DrawMap(this.GenerateCurves(data));
+      },
+      error => {
+        this.alertify.message(
+          "In order to see your flights displayed on a map, add some of them in a first place"
+        );
+        this.DrawMap(new Vector());
+      }
     );
-
-
   }
-  GenerateCurves(flightsData: any){
-    
-    var AirportsVisited=new Collections.Set(String);
-    this.vector=new Vector();
-    this.format=new GeoJSON({
-      featureProjection:"EPSG:3857"
-    })
+
+  GenerateCurves(flightsData: any) {
+    var AirportsVisited = new Collections.Set(String);
+    this.vector = new Vector();
+    this.format = new GeoJSON({
+      featureProjection: "EPSG:3857"
+    });
     flightsData.forEach(Flight => {
       AirportsVisited.add(Flight.departureAirport.acronym);
       AirportsVisited.add(Flight.destinationAirport.acronym);
       this.NumberOfFlights++;
-      this.DistanceTraveled+=sphere.getDistance([Flight.departureAirport.longitude,Flight.departureAirport.latitude],[Flight.destinationAirport.longitude,Flight.destinationAirport.latitude]);
-      var generator = new arcjs.GreatCircle({x: Flight.departureAirport.longitude, y: Flight.departureAirport.latitude},{x: Flight.destinationAirport.longitude, y: Flight.destinationAirport.latitude});
+      this.DistanceTraveled += sphere.getDistance(
+        [
+          Flight.departureAirport.longitude,
+          Flight.departureAirport.latitude],
+        [
+          Flight.destinationAirport.longitude,
+          Flight.destinationAirport.latitude
+        ]
+      );
+      var generator = new arcjs.GreatCircle(
+        {
+          x: Flight.departureAirport.longitude,
+          y: Flight.departureAirport.latitude
+        },
+        {
+          x: Flight.destinationAirport.longitude,
+          y: Flight.destinationAirport.latitude
+        }
+      );
       var n = 50; // n of points
       var coords = generator.Arc(n).geometries[0].coords;
-      var geojson = {"type":"Feature","geometry":{"type":"LineString","coordinates": coords},"properties":null };
+      var geojson = {
+        type: "Feature",
+        geometry: { type: "LineString", coordinates: coords },
+        properties: null
+      };
       this.vector.addFeatures(this.format.readFeatures(geojson));
     });
-    this.NumberOfAirportsVisited=AirportsVisited.size();
+    this.NumberOfAirportsVisited = AirportsVisited.size();
     this.FormatDistance();
-    return this.vector; 
+    return this.vector;
   }
-  FormatDistance(){
+  FormatDistance() {
     if (this.DistanceTraveled >= 1000) {
-        this.DistanceTraveled = (Math.round(this.DistanceTraveled / 1000 * 100) / 100) +
-        ' ' + 'km';
+      this.DistanceTraveled =
+        Math.round((this.DistanceTraveled / 1000) * 100) / 100 + " " + "km";
     } else {
-      this.DistanceTraveled = Math.round(this.DistanceTraveled) +
-        ' ' + 'm';
+      this.DistanceTraveled = Math.round(this.DistanceTraveled) + " " + "m";
     }
   }
-  DrawMap(vector: Vector){
+  DrawMap(vector: Vector) {
     this.layer = new OlVectorLayer({
       source: this.vector,
       style: new Style({
         stroke: new Stroke({
-          color: 'red',
+          color: "red",
           width: 3
         })
       })
@@ -118,14 +137,13 @@ export class MapComponent implements OnInit {
         }),
         this.layer
       ],
-      target: 'map',
+      target: "map",
       view: new OlView({
-        center: [0,0],
+        center: [0, 0],
         zoom: 2.5,
-        minZoom:2,
-        maxZoom:19
+        minZoom: 2,
+        maxZoom: 19
       })
     });
   }
-
 }
