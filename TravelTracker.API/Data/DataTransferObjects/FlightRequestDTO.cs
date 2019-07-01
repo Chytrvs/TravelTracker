f5a325f4b2a76682bc74f2543776c415afc98e36
Chytrvs <marcinchytrzynski@gmail.com>
2019-07-01 22:03:13 +0200

    DTO's code cleanup

--- TravelTracker.API/Data/DataTransferObjects/FlightRequestDTO.cs
+++ TravelTracker.API/Data/DataTransferObjects/FlightRequestDTO.cs
@@ -5,15 +5,12 @@ namespace TravelTracker.API.Data.DataTransferObjects
     public class FlightRequestDTO
     {   
         [Required]
-        //[RegularExpression("^[A-Z0-9]{3}$",ErrorMessage="Acronym is invalid, please use capital letters and numbers.")]
         public string Username { get; set; }
         [Required]
         [StringLength(4),MinLength(4)]
-        //[RegularExpression("^[A-Z0-9]{3}$",ErrorMessage="Acronym is invalid, please use capital letters and numbers.")]
         public string DepartureAirportAcronym { get; set; }
         [Required]
         [StringLength(4),MinLength(4)]
-        //[RegularExpression("^[A-Z0-9]{3}$",ErrorMessage="Acronym is invalid, please use capital letters and numbers.")]
         public string DestinationAirportAcronym { get; set; }
     }
 }
--- TravelTracker.API/Data/DataTransferObjects/UsernameDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace TravelTracker.API.Data.DataTransferObjects
-{
-    public class UsernameDTO
-    {
-        public string Username { get; set; }
-    }
-}
