bd10c6916f3662cb01520f53b7e8999ef13dd3b2
Chytrvs <marcinchytrzynski@gmail.com>
2020-02-05 20:56:58 +0100

    Changed user interface properties to lowercase since uppercase names caused problems when object was being assigned https://stackoverflow.com/questions/50673818/how-do-i-ensure-typescript-interface-properties-are-uppercase-after-i-obtain-res

--- TravelTrackerFront/src/app/interfaces/user.ts
+++ TravelTrackerFront/src/app/interfaces/user.ts
@@ -1,8 +1,8 @@
 import { Airport } from './airport';
 
 export interface User {
-    Username:string;
-    Email:string;
-    Password:string;
-    FavouriteAirport:Airport;
+    username:string;
+    email:string;
+    password:string;
+    favouriteAirport:Airport;
 }
